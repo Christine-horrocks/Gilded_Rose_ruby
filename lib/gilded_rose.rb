fc272729aec21cd9cbeea7d7ee499ffdcc6dd26e
Christine-horrocks <splogy2001@hotmail.com>
2019-01-02 17:35:07 +0000

    Metz refactor

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,7 +9,7 @@ class GildedRose
       if item.name == "Aged Brie"
         update_aged_brie(item)
       elsif item.name == "Sulfuras, Hand of Ragnaros"
-        update_sulfuras(item)
+        return
       elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
         update_backstage_pass(item)
       elsif item.name.downcase.include? "conjured"
@@ -22,72 +22,39 @@ class GildedRose
 
   private
     def update_average_item(item)
-      reduce_item_sell_in(item)
-      if item.quality > 0 && item.sell_in > 0
-        reduce_item_quality(item, 1)
-      elsif item.quality > 1 && item.sell_in < 0
-        reduce_item_quality(item, 2)
-      elsif item.quality == 1 && item.sell_in < 0
-        reduce_item_quality(item, 1)
-      else
-        item.quality = item.quality
-      end
-    end
+      item.sell_in -= 1
+      return if item.quality == 0
 
-    def update_sulfuras(item)
-      item.sell_in = item.sell_in
-      item.quality = item.quality
+      item.quality -= 1
+      item.quality -= 1 if item.sell_in <= 0 && item.quality >= 1
     end
 
     def update_aged_brie(item)
-      reduce_item_sell_in(item)
-      if item.quality < 50
-        add_item_quality(item, 1)
-      end
+      item.sell_in -= 1
+      return if item.quality >= 50
+
+      item.quality += 1
     end
 
     def update_backstage_pass(item)
-      reduce_item_sell_in(item)
-      if item.quality > 49
-        item.quality = item.quality
-      elsif item.sell_in < 0
-        item.quality = 0
-      elsif item.sell_in < 5
-        add_item_quality(item, 3)
-      elsif item.sell_in < 10
-        add_item_quality(item, 2)
-      else
-        add_item_quality(item, 1)
-      end
+      item.sell_in -= 1
+      return item.quality = 0 if item.sell_in < 0
+      return if item.quality >= 50
+
+      item.quality += 1
+      item.quality += 1 if item.sell_in < 10
+      item.quality += 1 if item.sell_in < 5
     end
 
     def update_conjured(item)
-      reduce_item_sell_in(item)
-      if item.quality > 1 && item.sell_in > 0
-        reduce_item_quality(item, 2)
-      elsif item.quality < 2 && item.sell_in > 0
-        item.quality = 0
-      elsif item.quality > 3 && item.sell_in < 0
-        reduce_item_quality(item, 4)
-      elsif item.quality < 4 && item.sell_in < 0
-        item.quality = 0
-      else
-        item.quality = item.quality
-      end
-    end
+      item.sell_in -= 1
+      return if item.quality == 0
 
-    def reduce_item_sell_in(item)
-      item.sell_in = item.sell_in - 1
+      item.quality -= 1
+      item.quality -= 1 if item.quality >= 1
+      item.quality -= 1 if item.sell_in < 1 && item.quality >= 1
+      item.quality -= 1 if item.sell_in < 1 && item.quality >= 1
     end
-
-    def add_item_quality(item, num)
-      item.quality = item.quality + (num)
-    end
-
-    def reduce_item_quality(item, num)
-      item.quality = item.quality - (num)
-    end
-
 end
 
 class Item
